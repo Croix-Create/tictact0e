b96f096d284350cf93154243ac07c7f4306907bd
Croix Frick <64685938+Croix-Create@users.noreply.github.com>
2024-06-03 11:05:00 +0300

    Updated script.js //  updatede turns

--- script.js
+++ script.js
@@ -64,7 +64,7 @@ function checkWinner(values) {
             return values[a];
         }
     }
-    return values.includes('') ? null : 'Match Tie';
+    return values.includes('') ? null : 'Tie';
 }
 
 function reset() { 
@@ -72,123 +72,8 @@ function reset() {
 	b1 = b2 = b3 = b4 = b5 = b6 = b7 = b8 = b9 = ''; 
 } 
 
-// Turn Checks
+var flag = 1;
 
-let flag = 1;
-
-function turn1() { 
-	if (flag == 1) { 
-		document.getElementById("b1").value = "X"; 
-		document.getElementById("b1").disabled = true; 
-		flag = 0; 
-	} 
-	else { 
-		document.getElementById("b1").value = "0"; 
-		document.getElementById("b1").disabled = true; 
-		flag = 1; 
-	} 
-} 
-
-function turn2() { 
-	if (flag == 1) { 
-		document.getElementById("b2").value = "X"; 
-		document.getElementById("b2").disabled = true; 
-		flag = 0; 
-	} 
-	else { 
-		document.getElementById("b2").value = "0"; 
-		document.getElementById("b2").disabled = true; 
-		flag = 1; 
-	} 
-} 
-
-function turn3() { 
-	if (flag == 1) { 
-		document.getElementById("b3").value = "X"; 
-		document.getElementById("b3").disabled = true; 
-		flag = 0; 
-	} 
-	else { 
-		document.getElementById("b3").value = "0"; 
-		document.getElementById("b3").disabled = true; 
-		flag = 1; 
-	} 
-} 
-
-function turn4() { 
-	if (flag == 1) { 
-		document.getElementById("b4").value = "X"; 
-		document.getElementById("b4").disabled = true; 
-		flag = 0; 
-	} 
-	else { 
-		document.getElementById("b4").value = "0"; 
-		document.getElementById("b4").disabled = true; 
-		flag = 1; 
-	} 
-} 
-
-function turn5() { 
-	if (flag == 1) { 
-		document.getElementById("b5").value = "X"; 
-		document.getElementById("b5").disabled = true; 
-		flag = 0; 
-	} 
-	else { 
-		document.getElementById("b5").value = "0"; 
-		document.getElementById("b5").disabled = true; 
-		flag = 1; 
-	} 
-} 
-
-function turn6() { 
-	if (flag == 1) { 
-		document.getElementById("b6").value = "X"; 
-		document.getElementById("b6").disabled = true; 
-		flag = 0; 
-	} 
-	else { 
-		document.getElementById("b6").value = "0"; 
-		document.getElementById("b6").disabled = true; 
-		flag = 1; 
-	} 
-} 
-
-function  turn7() { 
-	if (flag == 1) { 
-		document.getElementById("b7").value = "X"; 
-		document.getElementById("b7").disabled = true; 
-		flag = 0; 
-	} 
-	else { 
-		document.getElementById("b7").value = "0"; 
-		document.getElementById("b7").disabled = true; 
-		flag = 1; 
-	} 
-} 
-
-function turn8() { 
-	if (flag == 1) { 
-		document.getElementById("b8").value = "X"; 
-		document.getElementById("b8").disabled = true; 
-		flag = 0; 
-	} 
-	else { 
-		document.getElementById("b8").value = "0"; 
-		document.getElementById("b8").disabled = true; 
-		flag = 1; 
-	} 
-} 
-
-function turn9() { 
-	if (flag == 1) { 
-		document.getElementById("b9").value = "X"; 
-		document.getElementById("b9").disabled = true; 
-		flag = 0; 
-	} 
-	else { 
-		document.getElementById("b9").value = "0"; 
-		document.getElementById("b9").disabled = true; 
-		flag = 1; 
-	} 
-} 
+function changeTurn() {
+    flag = flag === 1 ? 0 : 1;
+}
